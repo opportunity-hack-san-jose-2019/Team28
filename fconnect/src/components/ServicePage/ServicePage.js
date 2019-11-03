77eacbd0f539e200516072f918711774f1c07185
Shivanireddy25 <shivanireddy.singireddy@sjsu.edu>
2019-11-03 01:27:04 -0700

    Few more changes

--- fconnect/src/components/ServicePage/ServicePage.js
+++ fconnect/src/components/ServicePage/ServicePage.js
@@ -24,26 +24,38 @@ import {Redirect} from 'react-router';
     console.log(this.state.courseId);
     axios.get(`${ROOT_URL}/services/categories/${this.state.courseId}`)
     .then((response)=>{
-    
+    console.log(response);
         this.setState({
             services: response.data
         })
-      
+        console.log(this.state.services);
     });
 
-    console.log(this.state.services);
-      
-     
 }
   
   render() {
       var services = this.state.services.map( data => {
-      return  <a href={data.URL}> </a>
+        return (  <div > 
+
+<div class="card" style = {{width :302 ,marginLeft: 5, marginRight : 5,height: 202, marginTop : 5, marginBottom : 5}}>
+<div class="card-body" style = {{ height : 130}}>
+</div>
+<h6 class="card-subtitle mb-2 text-muted" style= {{height : 50}}> <p> {data.Description} </p> </h6>
+ <p class="card-text"> <a href={data.URL}> {data.URL} </a> </p>
+</div>
+
+             
+              
+               
+          </div>
+        )
       }
       )
     return(
-        <div>
-{services}
+        <div class="ServicePage" style={{marginTop : 100}}>
+          
+            <p> <b> {this.state.courseId} Links </b></p>
+        {services}
 </div>
     );
    
