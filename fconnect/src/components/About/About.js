4fa09ef122d5d5a80f5fbf22c3b042ee50232e07
Karti <karti@gmail.com>
2019-11-03 10:43:40 -0800

    chat bot

--- fconnect/src/components/About/About.js
+++ fconnect/src/components/About/About.js
@@ -1,28 +1,36 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
-class About extends Component{
-
-    render() {
-        return (
-            <div className="py-5">
-                <div className="about container">
-                    <div className="row">
-                        <div className="px-5 col-md-8 text-center mx-auto">
-                            <h3 className="text-primary display-4"><b>Pingendo</b></h3>
-                            <h2 className="my-3">I am so happy, my dear friend.</h2>
-                            <p className="mb-3">I throw myself down among the tall grass by the trickling stream; and,
-                                as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear
-                                the buzz of the little world among the stalks, and grow familiar with the countless
-                                indescribable.</p> <a href="#" className="btn btn-primary">Download</a>
-                            <p className="mt-2">
-                                <small>Currently v2.0.3</small>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+class About extends Component {
+  render() {
+    return (
+      <div className="py-5">
+        <div className="about container">
+          <div className="row">
+            <div className="px-5 col-md-8 text-center mx-auto">
+              <h3 className="text-primary display-4">
+                <b>You are not alone!</b>
+              </h3>
+              <h2 className="my-3">
+                Let's connect together to help each other
+              </h2>
+              <p className="mb-3">
+                I throw myself down among the tall grass by the trickling
+                stream; and, as I lie close to the earth, a thousand unknown
+                plants are noticed by me: when I hear the buzz of the little
+                world among the stalks, and grow familiar with the countless
+                indescribable.
+              </p>{" "}
+              <iframe
+                width="650"
+                height="372"
+                src="https://www.youtube.com/embed/AxcsTezYcoc/?controls=1"
+              ></iframe>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default About;
--- fconnect/src/components/LandingPage/LandingPage.js
+++ fconnect/src/components/LandingPage/LandingPage.js
@@ -1,40 +1,24 @@
-
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Carousel } from "react-responsive-carousel";
 import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
-import logo from "../../images/Logo.png"
+import logo from "../../images/Logo.png";
 import Navbarhome from "../NavBar/Navbarhome";
-import {Route} from "react-router-dom";
-import SlideshowComponent from "../SlideShow/App"
+import { Route } from "react-router-dom";
+import SlideshowComponent from "../SlideShow/App";
 import Footer from "../Footer/Footer";
 import About from "../About/About";
+import Chatbot from "../Chatbot/CustomChatBot";
 
 export default class LandingPage extends Component {
   render() {
-
     return (
-        <div>
-            <SlideshowComponent />
-            <About />
-            <Footer />
+      <div>
+        <SlideshowComponent />
+        <About />
+        {/*<Footer />*/}
+        <Chatbot />
       </div>
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
